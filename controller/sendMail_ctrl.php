b72c54b3cf845b7db5eef688f066e1a89c89baa8
Monvoisin <monvoisin.pierre@gmail.com>
2020-07-21 08:00:13 +0200

    can send mail through gmail with my own email

--- controller/sendMail_ctrl.php
+++ controller/sendMail_ctrl.php
@@ -1,26 +1,26 @@
 <?php require_once '../share/forbiddenPages.php';
 require_once '../vendor/autoload.php';
 function sendMail($mailTo){
-  try {
-    // Setup transporter ( mailtrap.io is a virtual box)
-    $transport = (new Swift_SmtpTransport('smtp.mailtrap.io', 2525))
-    ->setUsername('e5e7afdff429fc')->setPassword('eb0ddeae9c189c');
-    // Initiate the mailer service
-    $mailer = new Swift_Mailer($transport);
-    // Create the message
-    $message = (new Swift_Message())
-    // Add subject
-    ->setSubject('First Test Mail')
-    //Put the From address
-    ->setFrom(['solution3.contact@gmail.com'])
-    // Include several To addresses
-    ->setTo([$mailTo => 'New user']);
-    // Include HTML
-    $message->setBody('<p>Welcome to Maitrap!</p>Now your test emails will be <i>safe</i>', 'text/html');
-    $message->addPart('Welcome to Mailtrap, now your test emails will be safe', 'text/plain');
-    // Send mail
-    $mailer->send($message);
-  } catch (PDOException $e) {
-    echo $e->getMessage();
+  // Setup transporter ( mailtrap.io is a virtual mailbox )
+  $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+  ->setUsername('monvoisin.pierre@gmail.com')->setPassword('mwynvhpbrwirlylk');
+  // Initiate the mailer service
+  $mailer = new Swift_Mailer($transport);
+  // Create the message
+  $message = (new Swift_Message())
+  // Add subject
+  ->setSubject('Demande de réinitialisation du mot de passe')
+  //Put the From address
+  ->setFrom(['solution3.contact@gmail.com' => 'Solution3.com'])
+  // Include several To addresses
+  ->setTo([$mailTo]);
+  // Include HTML
+  $message->setBody('<h3>Ceci est un mail de test envoyé depuis Solution³.com</h3>
+  <p>Veuillez ne pas y faire attention, mademoiselle</p>', 'text/html');
+  // Put text in case HTML don't load
+  $message->addPart('Ceci est un mail de test envoyé depuis Solution³.com. Veuillez ne pas y faire attention, mademoiselle', 'text/plain');
+  // Send mail
+  if ($mailer->send($message)){
+    // Mail sent, need redirection
   }
 } ?>
